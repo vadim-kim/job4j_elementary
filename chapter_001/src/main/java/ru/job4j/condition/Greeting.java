a6ef0a8f4fe2f3f6732a32298ec6d3849b1ac5d6
Vadim Kim <vadim.k1m@mail.ru>
2020-03-15 20:49:35 +0600

    Интеграция Git и IDEA [#159462]

--- chapter_001/src/main/java/ru/job4j/condition/Greeting.java
+++ chapter_001/src/main/java/ru/job4j/condition/Greeting.java
@@ -2,7 +2,9 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-    String idea="I like Java!" +"But i am a newbie";
+    String idea="I like Java!";
+    System.out.println(idea);
+    idea=idea+"But I am a newbie.";
     System.out.println(idea);
     int year=2020;
     String separate=" ";

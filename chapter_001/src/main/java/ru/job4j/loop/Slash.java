70e34c28db4105a341990b61227e4266e46b2119
Vadim Kim <vadim.k1m@mail.ru>
2020-03-30 15:11:49 +0600

    5.9. Крест в псевдографике. [#239331]

--- chapter_001/src/main/java/ru/job4j/loop/Slash.java
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -6,9 +6,7 @@ public class Slash {
             for (int cell = 0; cell < size; cell++) {
                 boolean left = row==cell;
                 boolean right = row+cell==size-1;
-                if (left) {
-                    System.out.print("0");
-                } else if (right) {
+                if (left||right) {
                     System.out.print("0");
                 } else {
                     System.out.print(" ");

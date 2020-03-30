18a62183e5c1dd364af1ac1b32cbfa1cf2efd964
Vadim Kim <vadim.k1m@mail.ru>
2020-03-30 18:14:59 +0600

    6.0.2. Заполнение массива. [#239271]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages=new short[10];
         String[] surnames=new String[100500];
         float[] prices=new float[40];
+
+        String[] names=new String[4];
+        names[0]="Vadim Kim";
+        names[1]="Melnikov Kirill";
+        names[2]="Mos'ko Ivan";
+        names[3]="Stolyarov Kirill";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }

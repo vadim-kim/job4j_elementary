bb8d82e0967c4f52c44ae7f485b3c76771f23f19
Vadim Kim <vadim.k1m@mail.ru>
2020-04-01 20:40:59 +0600

    6.4.1. Слово заканчивается с ... [#239263]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int index=post.length-1; index>0; index--) {
+            if (post[post.length-index]!=word[word.length-index]) {
+                result=false;
+            }
+        }
+        return result;
+    }
+}

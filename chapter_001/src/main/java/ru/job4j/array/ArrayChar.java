72fb2d86bd00a91f48c633fa5f7ed1b75741f8c3
Vadim Kim <vadim.k1m@mail.ru>
2020-04-06 20:20:18 +0600

    6.4. Слово начинается с ... [#239259]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,6 +6,7 @@ public class ArrayChar {
         for (int index=0; index<pref.length; index++) {
             if (pref[index]!=word[index]) {
                 result=false;
+                break;
             }
         }
         return result;
